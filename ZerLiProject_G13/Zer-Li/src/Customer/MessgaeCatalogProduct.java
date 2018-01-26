429a70f3cd4b48ebefa856b48c9f6c158156028d
HobbitWarrior <HobbitWarrior@users.noreply.github.com>
2018-01-27 01:12:45 +0200

    java doc messageCatalogProduct

--- ZerLiProject_G13/Zer-Li/src/Customer/MessgaeCatalogProduct.java
+++ ZerLiProject_G13/Zer-Li/src/Customer/MessgaeCatalogProduct.java
@@ -2,22 +2,38 @@ package Customer;
 
 import java.io.Serializable;
 
-public class MessgaeCatalogProduct implements Serializable	/**this is a message class to get catalog items of a branch!!*/
+public class MessgaeCatalogProduct implements Serializable	
 {
 	private final String msg = "Give Me All CatalogItems";
 	private String branchID="";
+	/**
+	 * Constructor, get id of branch
+	 * @param branchID
+	 */
 	public MessgaeCatalogProduct(String branchID) 
 	{
 		this.branchID = branchID;
 	}
+	/**
+	 * getter of id of branch
+	 * @return int id
+	 */
 	public String getBranchID() 
 	{
 		return branchID;
 	}
+	/**
+	 * setter of branch id
+	 * @param branchID
+	 */
 	public void setBranchID(String branchID) 
 	{
 		this.branchID = branchID;
 	}
+	/**
+	 * getter of message to server
+	 * @return string message to server
+	 */
 	public String getMsg() 
 	{
 		return msg;
