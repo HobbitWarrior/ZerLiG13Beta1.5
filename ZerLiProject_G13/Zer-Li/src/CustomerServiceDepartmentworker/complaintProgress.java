e808a13f452dd4bb785a6cef81c8b0788fdf6851
HobbitWarrior <wolfazb@gmail.com>
2018-01-26 11:43:48 +0200

    complaintProgress entry

---
+++ ZerLiProject_G13/Zer-Li/src/CustomerServiceDepartmentworker/complaintProgress.java
@@ -0,0 +1,66 @@
+package CustomerServiceDepartmentworker;
+
+public class complaintProgress {
+
+	/**
+	 * <h1>complaintProgress Fileds</h>
+	 * <p>
+	 * keeps the entries to be sent to the DB
+	 * 
+	 * @author Alex
+	 *         </p>
+	 */
+	private int ComplaintID;
+	private int EmpHandlingID;
+	private String Topic;
+	private String Details;
+
+	public complaintProgress(int complaintID, int empHandlingID, String topic, String details) {
+		// set the fields
+		setComplaintID(complaintID);
+		setEmpHandlingID(empHandlingID);
+		setTopic(topic);
+		setDetails(details);
+	}
+
+	public int getComplaintID() {
+		return ComplaintID;
+	}
+
+	public void setComplaintID(int complaintID) {
+		this.ComplaintID = complaintID;
+	}
+
+	public int getEmpHandlingID() {
+		return EmpHandlingID;
+	}
+
+	public void setEmpHandlingID(int empHandlingID) {
+		EmpHandlingID = empHandlingID;
+	}
+
+	public java.util.Date getDate() {
+		return Date;
+	}
+
+	public void setDate(java.util.Date date) {
+		Date = date;
+	}
+
+	public String getTopic() {
+		return Topic;
+	}
+
+	public void setTopic(String topic) {
+		Topic = topic;
+	}
+
+	public String getDetails() {
+		return Details;
+	}
+
+	public void setDetails(String details) {
+		Details = details;
+	}
+
+}
