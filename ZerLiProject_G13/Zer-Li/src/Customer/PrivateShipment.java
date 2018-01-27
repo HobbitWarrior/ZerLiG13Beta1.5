1a12c7e2e1802bada3611aaf5f73e4b1edf83804
HobbitWarrior <HobbitWarrior@users.noreply.github.com>
2018-01-27 02:57:23 +0200

    private shipment javadoc

--- ZerLiProject_G13/Zer-Li/src/Customer/PrivateShipment.java
+++ ZerLiProject_G13/Zer-Li/src/Customer/PrivateShipment.java
@@ -9,6 +9,15 @@ public class PrivateShipment extends Delivery implements Serializable
 	private String address;
 	private String phoneNumber;
 	
+	/**
+	  * constructor in case order id  and delivery id are known
+	 * @param OrderID id of order
+	 * @param DeliveryID id of delivery
+	 * @param Price price of shipment
+	 * @param addressee the person name that we send to him
+	 * @param address the address of the addressee
+	 * @param phoneNumber phone number of addressee
+	 */
 	public PrivateShipment(int OrderID, int DeliveryID, double Price, String addressee, String address, String phoneNumber ) 
 	{
 		super(OrderID, DeliveryID, Price);
@@ -16,7 +25,13 @@ public class PrivateShipment extends Delivery implements Serializable
 		this.address=address;
 		this.phoneNumber=phoneNumber;
 	}
-	
+	/**
+	 * constructor in case order id  and delivery id are unknown
+	 * @param Price price of shipment
+	 * @param addressee the person name that we send to him
+	 * @param address the address of the addressee
+	 * @param phoneNumber phone number of addressee
+	 */
 	public PrivateShipment( double Price, String addressee, String address, String phoneNumber ) 
 	{
 		super( Price);
@@ -24,37 +39,63 @@ public class PrivateShipment extends Delivery implements Serializable
 		this.address=address;
 		this.phoneNumber=phoneNumber;
 	}
-
+	/**
+	 * getter of addressee name
+	 * @return string
+	 */
 	public String getAddressee() 
 	{
 		return addressee;
 	}
 
+	/**
+	 * setter of addressee name
+	 * @param addressee string
+	 */
 	public void setAddressee(String addressee) 
 	{
 		this.addressee = addressee;
 	}
 
+	/**
+	 * getter of address of addressee
+	 * @return string
+	 */
 	public String getAddress() 
 	{
 		return address;
 	}
 
+	/**
+	 * setter of adress of addressee
+	 * @param address string new address
+	 */
 	public void setAddress(String address) 
 	{
 		this.address = address;
 	}
 
+	/**
+	 * getter phone number of addressee
+	 * @return string
+	 */
 	public String getPhoneNumber() 
 	{
 		return phoneNumber;
 	}
 
+	/**
+	 * setter phone number of a
+	 * @param phoneNumber
+	 */
 	public void setPhoneNumber(String phoneNumber) 
 	{
 		this.phoneNumber = phoneNumber;
 	}
 
+	/**
+	 * to string method, print important detail of shipment
+	 */
 	@Override
 	public String toString() {
 		return "PrivateShipment [addressee=" + addressee + ", address=" + address + ", phoneNumber=" + phoneNumber
