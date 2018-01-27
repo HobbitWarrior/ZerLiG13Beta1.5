72cb67d922622551a12d92796330ce2ac3ce419d
HobbitWarrior <wolfazb@gmail.com>
2018-01-27 22:57:30 +0200

    revenueReport JavaDoc

--- ZerLiProject_G13/Zer-Li/src/BranchManager/revenueReport.java
+++ ZerLiProject_G13/Zer-Li/src/BranchManager/revenueReport.java
@@ -1,21 +1,47 @@
 package BranchManager;
-
+/** An entity method that contains all the Revenue Report data.
+ * 
+ * @author Elias
+ *
+ */
 public class revenueReport {
 	
 	int OrderID;
 	double OrderPrice;
+	/**a getter method for the orderID
+	 * 
+	 * @return int OrderID
+	 */
 	public int getOrderID() {
 		return OrderID;
 	}
+	
+	/**a setter method that saves the order ID.
+	 * 
+	 * @param orderID
+	 */
 	public void setOrderID(int orderID) {
 		OrderID = orderID;
 	}
+	
+	/**a getter method that returns the current order's price.
+	 *  
+	 * @return double OrderPrice
+	 */
 	public double getOrderPrice() {
 		return OrderPrice;
 	}
+	
+	/** a setter method that returns the current order price.
+	 * 
+	 * @param orderPrice
+	 */
 	public void setOrderPrice(double orderPrice) {
 		OrderPrice = orderPrice;
 	}
+	/**a method that returns the string representation of the revenueReport class.
+	 * @override String toSting() of Object class.
+	 */
 	@Override
 	public String toString() {
 		return "revenueReport [OrderID=" + OrderID + ", OrderPrice=" + OrderPrice + "]";
@@ -25,6 +51,10 @@ public class revenueReport {
 		OrderID = orderID;
 		OrderPrice = orderPrice;
 	}
+	
+	/** revenueReport constructor.
+	 * 
+	 */
 	public revenueReport() {
 		super();
 	}
