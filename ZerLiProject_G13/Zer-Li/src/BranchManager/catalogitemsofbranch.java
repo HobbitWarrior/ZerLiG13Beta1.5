cf88a5d1d396768e94ed0a825187732b3d4d73d1
HobbitWarrior <HobbitWarrior@users.noreply.github.com>
2018-01-27 22:33:50 +0200

    catalog item of branch java doc

--- ZerLiProject_G13/Zer-Li/src/BranchManager/catalogitemsofbranch.java
+++ ZerLiProject_G13/Zer-Li/src/BranchManager/catalogitemsofbranch.java
@@ -2,34 +2,74 @@ package BranchManager;
 
 import java.io.Serializable;
 
-public class catalogitemsofbranch implements Serializable {
-
+/**
+ * class of catalog product of specific branch
+ * @author Elias qubety
+ *
+ */
+public class catalogitemsofbranch implements Serializable 
+{
 	
 	int ItemID;
 	String BranchID;
 	Double Price;
-	public int getItemID() {
+	/**
+	 * getter of id of product
+	 * @return integer id of product
+	 */
+	public int getItemID() 
+	{
 		return ItemID;
 	}
-	public void setItemID(int itemID) {
+	/**
+	 * setter of id of product
+	 * @param itemID integer new id of item
+	 */
+	public void setItemID(int itemID) 
+	{
 		ItemID = itemID;
 	}
-	public String getBranchID() {
+	/**
+	 * getter of id of branch
+	 * @return string id of branch
+	 */
+	public String getBranchID() 
+	{
 		return BranchID;
 	}
-	public void setBranchID(String branchID) {
+	/**
+	 * setter of id of branch
+	 * @param branchID string new id of branch
+	 */
+	public void setBranchID(String branchID) 
+	{
 		BranchID = branchID;
 	}
-	public Double getPrice() {
+	/**
+	 * getter of price of product
+	 * @return double price of product
+	 */
+	public Double getPrice() 
+	{
 		return Price;
 	}
-	public void setPrice(Double price) {
+	/**
+	 * setter of new price
+	 * @param price double new price for product
+	 */
+	public void setPrice(Double price) 
+	{
 		Price = price;
 	}
+	/**
+	 * to string method, return string of all details of product
+	 */
 	@Override
-	public String toString() {
+	public String toString() 
+	{
 		return "catalogitemsofbranch [ItemID=" + ItemID + ", BranchID=" + BranchID + ", Price=" + Price + "]";
 	}
+	
 	public catalogitemsofbranch(int itemID, String branchID, Double price) {
 		super();
 		ItemID = itemID;
