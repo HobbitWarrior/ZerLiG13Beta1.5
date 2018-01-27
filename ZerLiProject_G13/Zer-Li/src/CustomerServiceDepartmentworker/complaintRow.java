e11f32b93bfad118ac3404ae1b21bcf9262ea960
HobbitWarrior <wolfazb@gmail.com>
2018-01-28 01:51:36 +0200

    you know what it is

--- ZerLiProject_G13/Zer-Li/src/CustomerServiceDepartmentworker/complaintRow.java
+++ ZerLiProject_G13/Zer-Li/src/CustomerServiceDepartmentworker/complaintRow.java
@@ -11,61 +11,102 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
-    
+ /**a complaint row for the GUI in the  CustomerServiceDepartmentworkerMainWindow window
+  * used for binding to the fields  
+  * @author Alex
+  *
+  */
 public class complaintRow {
 
 	public Stage mainstage;
 	private StringProperty labelText;
 	private StringProperty timerText;
 
-	// complaint opening time
+	/** complaint opening time
+	 * 
+	 */
 	private int hours = 0;
+	/** complaint opening time
+	 * 
+	 */
 	private int minutes = 0;
+	/** complaint opening time
+	 * 
+	 */
 	private int seconds = 0;
 
-	//saves the current location of the complaint the observable list 'upgradedList'
+	/**saves the current location of the complaint the observable list 'upgradedList'
+	 * 
+	 */
 	public int ComplaintIndex=-1;
 	
-	
+	/**empty constructor initiates the variables with dafeult values
+	 * 
+	 */
 	public complaintRow() {
 		// default Values
 		labelText = new SimpleStringProperty(" ");
 		timerText = new SimpleStringProperty("00:20");
 
-		// generate random time, demi values for demo
+		/** generate random time, demi values for demo
+		 * 
+		 */
 		Random rn = new Random();
 		hours = rn.nextInt(24);
 		minutes = rn.nextInt(60);
 		seconds = rn.nextInt(60);
 	}
-
+/**a constructor 
+ * also calls the empty constructor for some basic settings
+ * 
+ * @param complaintText
+ * @param complaintIndex
+ * @param time
+ * @param stg
+ */
 	public complaintRow(String complaintText,int complaintIndex, String time, Stage stg) {
 		this(stg);
 		ComplaintIndex=complaintIndex;
 		complaintLabelSetter(complaintText);
 		GetComplaintOpeningTime(time);
 	}
-
+/**constructor 
+ * 
+ * @param stg
+ */
 	public complaintRow(Stage stg) {
 		this();
 		mainstage = stg;
 
 	}
 
-	// List row elements binding
+	/** List row elements binding
+	 * a getter for the complaint label
+	 * @return Label
+	 */
 	public StringProperty complaintLabelGetter() {
 		return labelText;
 	}
-
+/**timerText getter
+ * 
+ * @return timerText StringProperty
+ */
 	public StringProperty timerTextGetter() {
 		return timerText;
 	}
-
+/**complaintLabel setter 
+ * @param str string
+ */
 	public void complaintLabelSetter(String str) {
 		if (!str.isEmpty())
 			labelText.set(str);
 	}
-
+/**a timer text setter,
+ * gets the time in Hours, Minutes and seconds and sets it in the proper fields
+ * @param Hours
+ * @param Minutes
+ * @param Seconds
+ */
 	public void timerTextSetter(String Hours, String Minutes, String Seconds) {
 
 		int hh =Math.abs(Integer.parseInt(Hours) - hours);
@@ -84,7 +125,10 @@ public class complaintRow {
 		timerText.set(HH + hh + ":" + MM + mm + " : " + SS + ss);
 	}
 
-	//parse time to 'hours','minutes' & 'seconds' of type int
+	/**parse time to 'hours','minutes' & 'seconds' of type int
+	 * 
+	 * @param time
+	 */
 	public void GetComplaintOpeningTime(String time) {
 
 		String[] Time=time.split(":");
--- ZerLiProject_G13/Zer-Li/src/CustomerServiceDepartmentworker/expertReport.java
+++ ZerLiProject_G13/Zer-Li/src/CustomerServiceDepartmentworker/expertReport.java
@@ -5,11 +5,18 @@ import java.io.Serializable;
  * used for the client-server communication. 
  * 
  * @author Alex
+ * @implements Serializable
  *
  */
 public class expertReport implements Serializable  {
 	
-	
+/**constructor 
+ * 	
+ * @param ExpertId type of int
+ * @param Quarter type of int
+ * @param Year type of int
+ * @param Report type of String
+ */
 	public expertReport(int ExpertId,int Quarter,int Year,String Report)
 	{
 		setExpertID(ExpertId);
@@ -22,24 +29,52 @@ public class expertReport implements Serializable  {
 	private int quarter;
 	private int year;
 	private String report;
+	/**expert id getter
+	 * 
+	 * @return expertID
+	 */
 	public int getExpertID() {
 		return expertID;
 	}
+	/** expertID setter
+	 * 
+	 * @param expertID type of int
+	 */
 	public void setExpertID(int expertID) {
 		this.expertID = expertID;
 	}
+	/**reports quartet getter
+	 * 
+	 * @return quarter type of int
+	 */
 	public int getQuarter() {
 		return quarter;
 	}
+	/**quarter setter 
+	 * 
+	 * @param quarter
+	 */
 	public void setQuarter(int quarter) {
 		this.quarter = quarter;
 	}
+	/**year getter
+	 * 
+	 * @return year
+	 */
 	public int getYear() {
 		return year;
 	}
+	/**year setter
+	 * 
+	 * @param year
+	 */
 	public void setYear(int year) {
 		this.year = year;
 	}
+	/**report getter
+	 * 
+	 * @return report
+	 */
 	public String getReport() {
 		return report;
 	}
