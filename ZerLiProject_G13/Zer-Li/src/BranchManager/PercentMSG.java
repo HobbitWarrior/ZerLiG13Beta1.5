214360384ad703c68082dcd9759d0d84c046f2f4
HobbitWarrior <HobbitWarrior@users.noreply.github.com>
2018-01-27 23:52:23 +0200

    java doc percentMSG

--- ZerLiProject_G13/Zer-Li/src/BranchManager/PercentMSG.java
+++ ZerLiProject_G13/Zer-Li/src/BranchManager/PercentMSG.java
@@ -1,39 +1,87 @@
 package BranchManager;
 
 import java.io.Serializable;
-
-public class PercentMSG implements Serializable {
-
+/**
+ *  class of amount of discount on specific item in specific branch
+ * @author Elias qubeti
+ */
+public class PercentMSG implements Serializable 
+{
 	String itemId;
 	String Percent ;
 	String BranchID;
+	
+	/**
+	 * constructor 
+	 * @param itemId string id of item
+	 * @param percent string percentages of discount
+	 * @param BranchID string id of branch
+	 */
 	public PercentMSG(String itemId, String percent, String BranchID) 
 	{
 		this.itemId = itemId;
 		Percent = percent;
 		this.BranchID=BranchID;
 	}
-	public String getItemId() {
+	
+	/**
+	 * getter of id of item
+	 * @return string id of item
+	 */
+	public String getItemId() 
+	{
 		return itemId;
 	}
-	public void setItemId(String itemId) {
+	
+	/**
+	 * setter of id of item
+	 * @param itemId string new id for product
+	 */
+	public void setItemId(String itemId) 
+	{
 		this.itemId = itemId;
 	}
-	public String getPercent() {
+	
+	/**
+	 * getter of amount in percentage of discount
+	 * @return string amount of discount
+	 */
+	public String getPercent() 
+	{
 		return Percent;
 	}
-	public void setPercent(String percent) {
+	
+	/**
+	 * setter of percentage of discount
+	 * @param percent  string new percentage
+	 */
+	public void setPercent(String percent) 
+	{
 		Percent = percent;
 	}
 
+	/**
+	 * to string method, return string with all important details
+	 */
 	@Override
-	public String toString() {
+	public String toString() 
+	{
 		return "PercentMSG [itemId=" + itemId + ", Percent=" + Percent + "]";
 	}
-	public String getBranchID() {
+	/**
+	 * getter of id of branch
+	 * @return string
+	 */
+	public String getBranchID() 
+	{
 		return BranchID;
 	}
-	public void setBranchID(String branchID) {
+	/**
+	 * setter of id of branch
+	 * @param branchID string 
+	 */
+	public void setBranchID(String branchID) 
+	{
 		BranchID = branchID;
 	}
 	
