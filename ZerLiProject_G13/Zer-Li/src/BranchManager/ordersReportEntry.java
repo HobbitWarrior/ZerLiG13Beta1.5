717ddf53f3ff26df7ef429b0ab0945fc75abe12a
HobbitWarrior <HobbitWarrior@users.noreply.github.com>
2018-01-27 23:38:39 +0200

    order report entry java doc

--- ZerLiProject_G13/Zer-Li/src/BranchManager/ordersReportEntry.java
+++ ZerLiProject_G13/Zer-Li/src/BranchManager/ordersReportEntry.java
@@ -1,27 +1,53 @@
 package BranchManager;
-/***Holds each entry of the sales report ***/
-public class ordersReportEntry {
+/**
+ * Holds each entry of the sales report
+ * @author Elias qubety
+ *
+ */
+
+public class ordersReportEntry 
+{
 	private int itemType;
 	private int itemQuantity;
 	
+	/**
+	 * constructor 
+	 * @param ItemType integer type of report
+	 * @param ItemQuantity integer amount of items
+	 */
 	public ordersReportEntry(int ItemType,int ItemQuantity)
 	{
 		itemType=ItemType;
 		itemQuantity=ItemQuantity;
 	}
 
+	/**getter of item type
+	 * @return integer of item type
+	 */
 	public int getItemType() {
 		return itemType;
 	}
 
+	/**
+	 * setter of new item type
+	 * @param itemType integer 
+	 */
 	public void setItemType(int itemType) {
 		this.itemType = itemType;
 	}
-
-	public int getItemQuantity() {
+	/**
+	 * getter of amount of item
+	 * @return
+	 */
+	public int getItemQuantity() 
+	{
 		return itemQuantity;
 	}
 
+	/**
+	 * setter of amount of items
+	 * @param itemQuantity integer
+	 */
 	public void setItemQuantity(int itemQuantity) {
 		this.itemQuantity = itemQuantity;
 	}
