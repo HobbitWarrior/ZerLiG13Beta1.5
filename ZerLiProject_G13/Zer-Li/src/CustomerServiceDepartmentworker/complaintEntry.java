8abf1daaec5088654be77bc92f0170c509e3ccd3
HobbitWarrior <wolfazb@gmail.com>
2018-01-28 01:33:13 +0200

    kill me now :P

--- ZerLiProject_G13/Zer-Li/src/CustomerServiceDepartmentworker/complaintEntry.java
+++ ZerLiProject_G13/Zer-Li/src/CustomerServiceDepartmentworker/complaintEntry.java
@@ -20,7 +20,9 @@ public class complaintEntry {
 	public SimpleStringProperty details;
 	
 	
-	
+	/**complaint entry constructor
+	 * 
+	 */
 	public complaintEntry()
 	{
 		CompliantID=new SimpleIntegerProperty();
@@ -33,6 +35,10 @@ public class complaintEntry {
 		details=new SimpleStringProperty();
 		
 	}
+	/**complaint entry constructor that gets complaint
+	 * 
+	 * @param c
+	 */
 	public complaintEntry(complaint c)
 	{
 		this();
@@ -49,61 +55,133 @@ public class complaintEntry {
 	}
 	
 	
-	
+	/**complaintID getter
+	 * 
+	 * @return CompliantID
+	 */
 	public SimpleIntegerProperty getCompliantID() {
 		return CompliantID;
 	}
+	/**complaintID setter
+	 * 
+	 * @param compliantID
+	 */
 	public void setCompliantID(int compliantID) {
 		CompliantID.setValue(compliantID);
 	}
+	/**costumerID setter
+	 * 
+	 * @return SimpoleStringProperty
+	 */
 	public SimpleStringProperty getCustomerID() {
 		return new SimpleStringProperty(String.valueOf(CustomerID.getValue()));
 	}
+	/**customer ID getter of a different type
+	 * 
+	 * @return SimpleStringProperty
+	 */
 	public SimpleIntegerProperty getCustomerIDInteger()
 	{
 		return CustomerID;
 	}
-	
+	/**a costumerID setter
+	 * 
+	 * @param customerID type of int
+	 */
 	public void setCustomerID(int customerID) {
 		CustomerID.setValue(customerID);
 	}
+/**employee id getter
+ * 	
+ * @return SimpleStringProperty
+ */
 	public SimpleStringProperty getEmpHandlingIDString() {
 		return new SimpleStringProperty(String.valueOf(EmpHandlingID.getValue()));
 	}
+	
+	/**employeeID getter
+	 * 
+	 * @return SimpleIntegerProperty
+	 */
 	public SimpleIntegerProperty getEmpHandlingID()
 	{
 		return EmpHandlingID;
 	}
+	/** employee handling ID setter
+	 * 
+	 * @param empHandlingID
+	 */
 	public void setEmpHandlingID(int empHandlingID) {
 		EmpHandlingID.setValue(empHandlingID);
 	}
+	/**Topic getter
+	 * 
+	 * @return SimpleStringProperty
+	 */
 	public SimpleStringProperty getTopic() {
 		return Topic;
 	}
+	/**topic  setter
+	 * 
+	 * @param topic
+	 */
 	public void setTopic(String topic) {
 		Topic.setValue(topic);;
 	}
+	/** time getter 
+	 * 
+	 * @return SimpleStringProperty
+	 */
 	public SimpleStringProperty getTime() {
 		return Time;
 	}
+	/** time getter 
+	 * 
+	 * @param time
+	 */
 	public void setTime(String time) {
 		Time.setValue(time);
 	}
+	/**date getter
+	 * 
+	 * @return SimpleStringProperty
+	 */
 	public SimpleStringProperty getDate() {
 		return Date;
 	}
+	/**a setter for the date of the complaint
+	 * 
+	 * @param date String
+	 */
 	public void setDate(String date) {
 		Date.setValue(date);
 	}
+	/** a status of the copmplaint getter, can be 
+	 * open or closed
+	 * 
+	 * @return SimpleStringProperty
+	 */
 	public SimpleStringProperty getStatus() {
 		return Status;
 	}
+	/** a setter for the status
+	 * 
+	 * @param status SimpleStringProperty
+	 */
 	public void setStatus(String status) {
 		Status.setValue(status);
 	}
+	/** a details getter, saves the details of the
+	 * complaint
+	 * @return SimpleStringProperty
+	 */
 	public SimpleStringProperty getDetails() {
 		return details;
 	}
+	/** a setter of the details
+	 * 
+	 * @param details
+	 */
 	public void setDetails(String details) {
 		this.details.setValue(details);
 	}
