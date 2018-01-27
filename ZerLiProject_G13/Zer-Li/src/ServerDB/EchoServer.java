5348469aaf847725e066be1a75ab3254d25ed91a
HobbitWarrior <wolfazb@gmail.com>
2018-01-27 17:51:45 +0200

    fixes

--- ZerLiProject_G13/Zer-Li/src/ServerDB/EchoServer.java
+++ ZerLiProject_G13/Zer-Li/src/ServerDB/EchoServer.java
@@ -11,6 +11,7 @@ import java.sql.Time;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 import java.util.ResourceBundle;
 
