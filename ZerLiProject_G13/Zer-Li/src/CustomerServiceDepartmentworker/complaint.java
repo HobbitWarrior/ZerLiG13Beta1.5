223f374097d5cca4f926990ea5e5bf497beba8e6
HobbitWarrior <wolfazb@gmail.com>
2018-01-28 01:02:01 +0200

    complaint javadoc

--- ZerLiProject_G13/Zer-Li/src/CustomerServiceDepartmentworker/complaint.java
+++ ZerLiProject_G13/Zer-Li/src/CustomerServiceDepartmentworker/complaint.java
@@ -27,7 +27,17 @@ public class complaint implements Serializable {
 	private String Status;
 	private String Details;
 	public boolean newComplaint;
-
+/**complaint constructor
+ * 
+ * @param complaintID
+ * @param customerID
+ * @param empHandling
+ * @param topic
+ * @param timeComplaint
+ * @param dateComplaint
+ * @param status
+ * @param details
+ */
 	public complaint(int complaintID, int customerID, int empHandling, String topic, String timeComplaint,
 			String dateComplaint, String status, String details) {
 
@@ -41,69 +51,117 @@ public class complaint implements Serializable {
 		Status = status;
 		Details = details;
 	}
-
+/**a getter for the complaintID
+ * 
+ * @return ComplaintID
+ */
 	public int getComplaintID() {
 		return ComplaintID;
 	}
-
+/**a setter for the complaintID
+ * 
+ * @param complaintID
+ */
 	public void setComplaintID(int complaintID) {
 		ComplaintID = complaintID;
-		;
 	}
-
+/** a getter for the customerID
+ * 
+ * @return CustomerID
+ */
 	public int getCustomerID() {
 		return CustomerID;
 	}
-
+/** a setter for the customerID
+ * 
+ * @param customerID
+ */
 	public void setCustomerID(int customerID) {
 		CustomerID = customerID;
 		;
 	}
-
+/** a getter for the Employee Handling id
+ * 
+ * @return EmpHandling
+ */
 	public int getEmpHandling() {
 		return EmpHandling;
 	}
-
+/** a setter for the EmpHandling
+ * 
+ * @param empHandling
+ */
 	public void setEmpHandling(int empHandling) {
 		EmpHandling = empHandling;
 	}
-
+/** a getter for the Topic 
+ * 
+ * @return Topic type of String
+ */
 	public String getTopic() {
 		return Topic;
 	}
-
+/** a setter for the Topic
+ * 
+ * @param topic type of String
+ */
 	public void setTopic(String topic) {
 		Topic = topic;
 	}
-
+/**time complaint getter 
+ * 
+ * @return TimeComplaint
+ */ 
 	public String getTimeComplaint() {
 		return TimeComplaint;
 	}
-
+/**a getter for the time of the complaint
+ * 
+ * @param timeComplaint
+ */
 	public void setTimeComplaint(String timeComplaint) {
 		TimeComplaint = timeComplaint;
 	}
-
+/** a getter for the date of the complaint
+ * 
+ * @return DateComplaint
+ */
 	public String getDateComplaint() {
 		return DateComplaint;
 	}
-
+/** a setter for the DateCOmplaint
+ * 
+ * @param dateComplaint type of string
+ */
 	public void setDateComplaint(String dateComplaint) {
 		DateComplaint = dateComplaint;
 	}
-
+/** a getter for the status
+ * 
+ * @return Status
+ */
 	public String getStatus() {
 		return Status;
 	}
-
+/** a setter for the status.
+ * 
+ * @param status
+ */
 	public void setStatus(String status) {
 		Status = status;
 	}
-
+/** a setter for the details.
+ * 
+ * 
+ * @param details type of String
+ */
 	public void setDetails(String details) {
 		Details = details;
 	}
-
+/** a getter for the details
+ * 
+ * @return Details
+ */
 	public String getDetails() {
 		return Details;
 	}
